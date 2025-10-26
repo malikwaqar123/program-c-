adfc33a806470d8c3248b6b0d359ac87054be853
Malik Waqar Latif <waqarmalik2053@gmail.com>
2025-10-27 00:04:34 +0500

    Add files via upload

---
+++ program 1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Welcome to the c++ Lab!"<<endl;
+	cout<<"You will learn the basic output today."<<endl;
+	cout<<"Let's start with simple messages."<<endl;
+	return 0;
+}
