9cfc72e3115a4c62aae80b77ab86d2ff49658cd7
Malik Waqar Latif <waqarmalik2053@gmail.com>
2025-10-27 00:07:06 +0500

    Add files via upload

---
+++ program 4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"She said, \"Let's learn c++!\"\n";
+	cout<<"To start, save your file in c:\\c++_Projects\\Lab1\n";
+	cout<<"Good luck!";
+	return 0;
+}
