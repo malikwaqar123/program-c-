d2691ac74cba254b2f56bbd8ee5e8a3e7ddc1283
Malik Waqar Latif <waqarmalik2053@gmail.com>
2025-10-27 00:06:46 +0500

    Add files via upload

---
+++ program 3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Item\tPrice\tQuantity\n";
+	cout<<"Pen\t1$\t10\n";
+	cout<<"Pencil\t2$\t5\n";
+	cout<<"Eraser\t0.5$\t20\n";
+	return 0;
+}
